06b4d11720b880c3df9e13eedd841f506545beeb
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-08-19 10:38:55 +0200

    add new quote

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,33 @@ import '../styles/App.css';
 import friends from "../data/friends.json";
 import { useState } from "react";
 
+
 function App() {
 
   //Variables de estado
   const [data, setData] = useState(friends);
+  const [newQuote, setNewQuote] = useState({
+    quote: "",
+    character: ""
+  });
 
 
 
+  const handleNewQuote = (ev) => {
+    setNewQuote({
+      ...newQuote,
+      [ev.target.id]: ev.target.value
+    });
+  }
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setData([...data, newQuote]);
+    setNewQuote({
+      quote: "",
+      character: ""
+    });
+  }
 
 
 
@@ -25,17 +45,41 @@ function App() {
   });
 
   return (
-<div>
-<header>
-      <h1>Frases de Friends</h1>
-    </header>
+      <div>
+          <header>
+            <h1>Frases de Friends</h1>
+          </header>
 
-    <main>
-      <ul>
-        {htmlData}
-      </ul>
-    </main>
-</div>
+          <main>
+            <ul>
+              {htmlData}
+            </ul>
+
+            <form>
+              <h2>Añadir una nueva frase</h2>
+              <label>Frase</label>
+              <input
+              type="text"
+              name="quote"
+              id="quote"
+              value={newQuote.quote}
+              onChange ={handleNewQuote}
+              ></input>
+              <label>Personaje</label>
+              <input
+              type="text"
+              name="character"
+              id="character"
+              value={newQuote.character}
+              onChange ={handleNewQuote}
+              ></input>
+              <input
+              type="submit"
+              value="Añadir una nueva frase"
+              onClick={handleClick}></input>
+            </form>
+          </main>
+      </div>
  )
 
 
