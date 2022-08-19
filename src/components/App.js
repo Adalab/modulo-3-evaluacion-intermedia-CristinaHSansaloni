ea22993439593c195bba1463915c29f6bd086b74
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-08-19 09:40:23 +0200

    add data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 // Fichero src/components/App.js
 import '../styles/App.css';
+import friends from "../data/friends.json";
+import { useState } from "react";
 
 function App() {
+
+  //Variables de estado
+  const [data, setData] = useState(friends);
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <header>
+      <h1>Frases de Friends</h1>
+    </header>
   );
 }
 
